ad1345574f86d632a5f9a668f92f47db48301417
Dave Longley <dlongley@digitalbazaar.com>
2024-02-23 15:22:33 -0500

    Require `indexAllocator` when setting status.

--- schemas/bedrock-vc-status.js
+++ schemas/bedrock-vc-status.js
@@ -3,17 +3,27 @@
  */
 import {MAX_LIST_SIZE} from '../lib/constants.js';
 
+// an ID value required to unambiguously identify index allocation state
+const indexAllocator = {
+  // an ID (URL) referring to an index allocator
+  type: 'string',
+  // FIXME: pull in schema from bedrock-validation that uses
+  // `uri` pattern from ajv-formats once available
+  pattern: '^(.+):(.+)$'
+};
+
 export const createStatusListBody = {
   title: 'Create Status List',
   type: 'object',
   additionalProperties: false,
   required: [
-    'credentialId', 'type', 'indexAllocator', 'length', 'statusPurpose'
+    'credentialId', 'indexAllocator', 'type', 'length', 'statusPurpose'
   ],
   properties: {
     credentialId: {
       type: 'string'
     },
+    indexAllocator,
     type: {
       type: 'string',
       // supported types in this version
@@ -22,14 +32,6 @@ export const createStatusListBody = {
         // FIXME: consider removing `StatusList2021` support
         'StatusList2021'
       ]
-    },
-    // an ID value required to track index allocation
-    indexAllocator: {
-      // an ID (URL) referring to an index allocator
-      type: 'string',
-      // FIXME: pull in schema from bedrock-validation that uses
-      // `uri` pattern from ajv-formats once available
-      pattern: '^(.+):(.+)$'
     },
     // length of the status list in bits
     length: {
@@ -46,13 +48,13 @@ export const createStatusListBody = {
 export const updateCredentialStatusBody = {
   title: 'Update Credential Status',
   type: 'object',
-  // FIXME: consider if `indexAllocator` should be required
   required: ['credentialId', 'credentialStatus'],
   additionalProperties: false,
   properties: {
     credentialId: {
       type: 'string'
     },
+    indexAllocator,
     credentialStatus: {
       type: 'object',
       required: ['type', 'statusPurpose'],
